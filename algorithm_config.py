be7ff4f8f9eac39e4cbd78589cfe42ef9327a8a9
verachtertr <verachtertr@users.noreply.github.com>
2022-07-28 11:36:57 +0200

    Updated config

--- algorithm_config.py
+++ algorithm_config.py
@@ -3,6 +3,7 @@ from recpack.algorithms import TARSItemKNN, TARSItemKNNCoocDistance
 HOUR = 3600  # seconds
 DAY = 24 * HOUR
 
+SIMILARITY_FUNCTIONS = ["cosine", "pearson"]
 
 ALGORITHM_CONFIG = {
     # Literature algorithms
@@ -58,7 +59,7 @@ ALGORITHM_CONFIG = {
     "TARSItemKNNexponential": {
         "algorithm": "TARSItemKNN",
         "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
+            "similarity": SIMILARITY_FUNCTIONS,
             "fit_decay": [
                 1 / x
                 for x in [1 * HOUR, 2 * HOUR, 3 * HOUR, 6 * HOUR, 12 * HOUR, 1 * DAY, 7 * DAY, 14 * DAY, 30 * DAY]
@@ -75,7 +76,7 @@ ALGORITHM_CONFIG = {
     "TARSItemKNNlog": {
         "algorithm": "TARSItemKNN",
         "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
+            "similarity": SIMILARITY_FUNCTIONS,
             "fit_decay": [2, 4, 8, 16, 32],
             "predict_decay": [2, 4, 8, 16, 32],
             "decay_interval": [1, HOUR, DAY, 7 * DAY, 30 * DAY],
@@ -85,7 +86,7 @@ ALGORITHM_CONFIG = {
     "TARSItemKNNlinear": {
         "algorithm": "TARSItemKNN",
         "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
+            "similarity": SIMILARITY_FUNCTIONS,
             "fit_decay": [0.1, 0.3, 0.5, 0.7, 0.9, 1],
             "predict_decay": [0.1, 0.3, 0.5, 0.7, 0.9, 1],
             "decay_interval": [1, HOUR, DAY, 7 * DAY, 30 * DAY],
@@ -95,27 +96,17 @@ ALGORITHM_CONFIG = {
     "TARSItemKNNconcave": {
         "algorithm": "TARSItemKNN",
         "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
-            "fit_decay": [0.01, 0.1, 0.3, 0.9],
-            "predict_decay": [0.01, 0.1, 0.3, 0.9],
+            "similarity": SIMILARITY_FUNCTIONS,
+            "fit_decay": [0.1, 0.3, 0.5, 0.7, 0.8, 0.9],
+            "predict_decay": [0.1, 0.3, 0.5, 0.7, 0.8, 0.9],
             "decay_interval": [1, HOUR, DAY, 7 * DAY, 30 * DAY],
         },
         "params": {"decay_function": "concave"},
     },
-    "TARSItemKNNconvex": {
-        "algorithm": "TARSItemKNN",
-        "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
-            "fit_decay": [0.01, 0.1, 0.3, 0.9],
-            "predict_decay": [0.01, 0.1, 0.3, 0.9],
-            "decay_interval": [1, HOUR, DAY, 7 * DAY, 30 * DAY],
-        },
-        "params": {"decay_function": "convex"},
-    },
     "TARSItemKNNlinear_steeper": {
         "algorithm": "TARSItemKNN",
         "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
+            "similarity": SIMILARITY_FUNCTIONS,
             "fit_decay": [1, 5, 10, 50, 100, 1000],
             "predict_decay": [1, 5, 10, 50, 100, 1000],
             "decay_interval": [1, HOUR, DAY, 7 * DAY, 30 * DAY],
@@ -125,12 +116,12 @@ ALGORITHM_CONFIG = {
     "TARSItemKNNinverse": {
         "algorithm": "TARSItemKNN",
         "grid": {
-            "similarity": TARSItemKNN.SUPPORTED_SIMILARITIES,
+            "similarity": SIMILARITY_FUNCTIONS,
             "decay_interval": [1, HOUR, DAY, 7 * DAY, 30 * DAY],
         },
         "params": {"decay_function": "inverse"},
     },
-    #"TARSItemKNNCoocDistanceexponential": {
+    # "TARSItemKNNCoocDistanceexponential": {
     #    "algorithm": "TARSItemKNNCoocDistance",
     #    "grid": {
     #        "similarity": TARSItemKNNCoocDistance.SUPPORTED_SIMILARITIES,
@@ -147,7 +138,7 @@ ALGORITHM_CONFIG = {
     #        "event_age_weight": [0, 0.25, 0.5, 0.75, 1],
     #    },
     #    "params": {"decay_function": "exponential"},
-    #},
+    # },
     # "TARSItemKNNCoocDistancelog": {
     #     "algorithm": "TARSItemKNNCoocDistance",
     #     "grid": {
@@ -159,7 +150,7 @@ ALGORITHM_CONFIG = {
     #     },
     #     "params": {"decay_function": "log"},
     # },
-    #"TARSItemKNNCoocDistancelinear": {
+    # "TARSItemKNNCoocDistancelinear": {
     #    "algorithm": "TARSItemKNNCoocDistance",
     #    "grid": {
     #        "similarity": TARSItemKNNCoocDistance.SUPPORTED_SIMILARITIES,
@@ -169,7 +160,7 @@ ALGORITHM_CONFIG = {
     #        "event_age_weight": [0, 0.25, 0.5, 0.75, 1],
     #    },
     #    "params": {"decay_function": "linear"},
-    #},
+    # },
     # "TARSItemKNNCoocDistanceconcave": {
     #     "algorithm": "TARSItemKNNCoocDistance",
     #     "grid": {
@@ -203,7 +194,7 @@ ALGORITHM_CONFIG = {
     #     },
     #     "params": {"decay_function": "linear_steeper"},
     # },
-    #"TARSItemKNNCoocDistanceinverse": {
+    # "TARSItemKNNCoocDistanceinverse": {
     #    "algorithm": "TARSItemKNNCoocDistance",
     #    "grid": {
     #        "similarity": TARSItemKNNCoocDistance.SUPPORTED_SIMILARITIES,
@@ -211,5 +202,5 @@ ALGORITHM_CONFIG = {
     #        "event_age_weight": [0, 0.25, 0.5, 0.75, 1],
     #    },
     #    "params": {"decay_function": "inverse"},
-    #},
+    # },
 }
